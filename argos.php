5f66bbce41e923a2d83b3dec29bfcee771cbad22
nictodevcom <151468356+nictodevcom@users.noreply.github.com>
2024-07-19 19:08:05 +0700

    Create argos.php

---
+++ argos.php
@@ -0,0 +1,47 @@
+<?php
+
+function feedback4044()
+{
+    echo "Mohon Maaf Sudah Takedown";
+}
+
+function feedback40X4()
+{
+    echo "Blocked";
+}
+
+if (isset($_GET['sektor'])) {
+    $filename = "list.txt";
+    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $target_string = strtolower($_GET['sektor']);
+    foreach ($lines as $item) {
+        if (strtolower($item) === $target_string) {
+            $BRAND = strtoupper($target_string);
+        }
+    }
+    if (isset($BRAND)) {
+        $BRANDS = $BRAND;
+        echo $BRAND;
+        $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+        $fullUrl = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        if (isset($fullUrl)) {
+            $parsedUrl = parse_url($fullUrl);
+            $scheme = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] : '';
+            $host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
+            $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
+            $query = isset($parsedUrl['query']) ? $parsedUrl['query'] : '';
+            $baseUrl = $scheme . "://" . $host . $path . '?' . $query;
+            $urlPath = $baseUrl;
+        } else {
+            echo "URL saat ini tidak didefinisikan.";
+        }
+    } else {
+        feedback4044();
+        exit();
+    }
+} else {
+    feedback40X4();
+    exit();
+}
+
+?>
